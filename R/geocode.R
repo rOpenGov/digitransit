6e3172693b51076ca1a12cfe677f2bce818723b0
dieghernan <dieghernan@users.noreply.github.com>
2021-10-13 19:26:22 +0000

    Tidy code

--- R/geocode.R
+++ R/geocode.R
@@ -6,16 +6,16 @@
 #' @return Data frame with latitude and longitude of the place
 #' @examples
 #' geocode(string = "Eduskuntatalo", hits = 1)
-
-
 geocode <- function(string = "helsinki", hits = 1, host = "https://api.digitransit.fi/geocoding") {
   string <- gsub(" ", "%20", string)
-  resp <- httr::GET(paste0(host,"/v1/search?text=",string,"&size=",hits))
+  resp <- httr::GET(paste0(host, "/v1/search?text=", string, "&size=", hits))
   # print("Hello, world!")
   tmp <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
-  dat <- data.frame(string = string,
-             lon = tmp[[3]][[1]]$geometry$coordinates[[1]],
-             lat = tmp[[3]][[1]]$geometry$coordinates[[2]],
-             label = tmp[[3]][[1]]$properties$label)
+  dat <- data.frame(
+    string = string,
+    lon = tmp[[3]][[1]]$geometry$coordinates[[1]],
+    lat = tmp[[3]][[1]]$geometry$coordinates[[2]],
+    label = tmp[[3]][[1]]$properties$label
+  )
   return(dat)
 }
--- R/reverse_geocode.R
+++ R/reverse_geocode.R
@@ -7,18 +7,19 @@
 #' @return Data frame with information about the point
 #' @examples
 #' reverse_geocode(lon = "24.95126", lat = "60.15948", hits = 1)
-
 reverse_geocode <- function(lon = "24.95126", lat = "60.15948", hits = 1, host = "https://api.digitransit.fi/geocoding") {
-  resp <- httr::GET(paste0(host,"/v1/reverse?point.lat=",lat,"&point.lon=",lon,"&size=",hits))
+  resp <- httr::GET(paste0(host, "/v1/reverse?point.lat=", lat, "&point.lon=", lon, "&size=", hits))
   # print("Hello, world!")
   tmp <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
-  dat <- data.frame(lon = lon,
-                    lat = lat,
-             country = tmp[[3]][[1]]$properties$country,
-             country_a = tmp[[3]][[1]]$properties$country_a,
-             region = tmp[[3]][[1]]$properties$region,
-             localadmin = tmp[[3]][[1]]$properties$localadmin,
-             locality = tmp[[3]][[1]]$properties$locality,
-             label = tmp[[3]][[1]]$properties$label)
+  dat <- data.frame(
+    lon = lon,
+    lat = lat,
+    country = tmp[[3]][[1]]$properties$country,
+    country_a = tmp[[3]][[1]]$properties$country_a,
+    region = tmp[[3]][[1]]$properties$region,
+    localadmin = tmp[[3]][[1]]$properties$localadmin,
+    locality = tmp[[3]][[1]]$properties$locality,
+    label = tmp[[3]][[1]]$properties$label
+  )
   return(dat)
 }
